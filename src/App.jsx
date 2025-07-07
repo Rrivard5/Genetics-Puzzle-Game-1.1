1511bce38d137699664e447a5f67a0e911593eb5
Rrivard5 <rebecca.rivard@villanova.edu>
2025-07-07 15:49:07 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Room1 from './pages/Room1'
+import Room2 from './pages/Room2'
+import Room3 from './pages/Room3'
+import Room4 from './pages/Room4'
+import Completion from './pages/Completion'
+import { GameProvider } from './context/GameStateContext'
+
+export default function App() {
+  return (
+    <GameProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/room1" element={<Room1 />} />
+          <Route path="/room2" element={<Room2 />} />
+          <Route path="/room3" element={<Room3 />} />
+          <Route path="/room4" element={<Room4 />} />
+          <Route path="/complete" element={<Completion />} />
+        </Routes>
+      </Router>
+    </GameProvider>
+  )
+}
